fc6c05fa2701ec0ccbfc231e9286808d36b1da80
Daniel Farrugia <daniel.farrugia@globalcapital.com.mt>
2016-07-02 11:33:56 +0200

    1st Commit

---
+++ SQLElearner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SQLElearner.Startup))]
+namespace SQLElearner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
