113ee254fd9bc80740a8419f6759d75d7b1dd153
Daniel Farrugia <daniel.farrugia@globalcapital.com.mt>
2016-07-14 14:02:03 +0200

    Created the Course Model, View and Controller

---
+++ SQLElearner/Models/CourseModels.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace SQLElearner.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+    }
+
+    public class CourseDbContext :  DbContext
+    {
+        public CourseDbContext()
+            : base("DefaultConnection")
+        {
+        }
+        
+        public DbSet<Course> Courses { get; set; }
+    }
+}
